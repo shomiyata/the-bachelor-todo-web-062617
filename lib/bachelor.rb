5124ba29ac18c5a93c8a06c798cfee0ef00925a6
Sho Miyata <sho.miyata.1@gmail.com>
2017-06-06 05:33:08 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,59 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |season_key, contestant_array|
+    if season == season_key
+      contestant_array.each do |contestant_hash, contestant_hash_index|
+        if contestant_hash["status"] == "Winner"
+        return contestant_hash["name"].split(" ").first
+      end
+    end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season_key, contestant_array|
+      contestant_array.each do |contestant_hash, contestant_hash_index|
+        if contestant_hash["occupation"] == occupation
+        return contestant_hash["name"]
+    end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season_key, contestant_array|
+      contestant_array.each do |contestant_hash, contestant_hash_index|
+        if contestant_hash["hometown"] == hometown
+        counter += 1
+      end
+      end
+    end
+  return counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season_key, contestant_array|
+      contestant_array.each do |contestant_hash, contestant_hash_index|
+        if contestant_hash["hometown"] == hometown
+        return contestant_hash["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages_sum = 0
+  counter = 0
+  data.each do |season_key, contestant_array|
+    if season == season_key
+      contestant_array.each do |contestant_hash, contestant_hash_index|
+        ages_sum += contestant_hash["age"].to_f
+        counter += 1
+      end
+    end
+  end
+  return (ages_sum / counter).round
 end
